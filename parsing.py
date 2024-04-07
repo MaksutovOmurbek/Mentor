2adecdd43293ad02933e65f590aac25839065950
MaksutovOmurbek <maksutovomurbek164@gmail.com>
2024-04-07 18:12:34 +0300

    update

--- parsing.py
+++ parsing.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from aiogram import Bot, Dispatcher, executor, types
 import time, logging
+from datetime import datetime
+import asyncio
 
 
 Token= '7106836516:AAHdrX2n783ZcMNhVRaKnEEkt0uYCIaVL64'
@@ -17,17 +19,32 @@ async def start(message: types.Message):
     url = 'https://www.nbkr.kg/index.jsp?lang=RUS'
     response = requests.get(url)
     soup  = BeautifulSoup(response.text, 'lxml')
-    
-  
+    time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
     usd_rate = soup.find_all('td', class_='exrate')
     eur_rate = soup.find_all('td', class_='exrate')
     rub_rate = soup.find_all('td', class_='exrate')
+    time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
 
     for rub, usd, euro in zip(rub_rate, eur_rate, usd_rate ):
-        await message.answer(f"\n Доллар {usd} \n Евро {euro} \n Рубль {rub}")
+        await message.answer(f" Валюта {time}:\nКурс USD: {usd[0].text}\nКурс EUR: {euro[2].text}\nКурс RUB: {rub[4].text}\n")
     
 
+async def send_currency_rates_periodically(message):
+    while True:
+        await message.answer(message)
+        await asyncio.sleep(60)  
+
+
+async def on_startup(dispatcher):
+    pass
+
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.answer(message)
+    asyncio.create_task(send_currency_rates_periodically(message))
 
 
 executor.start_polling(dp)
